93adb1ff5cf4610e7e742e41d0c50ad60db5a902
Rony J. Letona <zronyj@Jean.(none)>
2014-01-05 14:24:54 -0600

    Adding an alternative to test.

--- chemserver.py
+++ chemserver.py
@@ -10,6 +10,10 @@ app = flask.Flask(__name__)
 
 logging.basicConfig(filename='server.log', format='%(asctime)s - %(message)s', filemode='w', level=logging.INFO)
 
+@app.route("/")
+def hello():
+    return "Hello World!"
+
 app.add_url_rule('/firefly/', view_func=Firefly.as_view('firefly'), methods=['GET', 'POST'])
 app.add_url_rule('/autodock/', view_func=AD4.as_view('autodock'), methods=['GET', 'POST'])
 app.add_url_rule('/advina/', view_func=ADVina.as_view('advina'), methods=['GET', 'POST'])
